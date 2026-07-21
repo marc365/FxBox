5e59a45bf1b56dbdcdd2946c6016d72fb3855662
Marc365 <marc365@github.com>
2026-07-21 23:14:15 +0100

    mp3

---
+++ build/extension/machinecode.s
@@ -0,0 +1,16 @@
+   org   0
+start:
+   asl.b    #1,d0
+   rts
+   asr.b    #1,d0
+   rts
+   lsr.b    #1,d0
+   rts
+   and.l    d1,d0
+   rts
+   asl.l    d1,d0
+   rts
+   asr.l    d1,d0
+   rts
+
+
